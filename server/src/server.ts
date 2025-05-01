ea393f972d705c69ce7d79a57add82b45ce186a7
lunah <lunah@farshads-air.lan>
2025-05-01 15:01:41 -0700

    Add path in server.ts to fix render deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -42,10 +42,10 @@ const startApolloServer = async () => {
 
   // Serve React\frontend build in production mode
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
+     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+   });
   }
 
   // Wait for MongoDB and Start listening
